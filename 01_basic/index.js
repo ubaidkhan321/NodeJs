9768887841ed95f7ac3a31f270c06caecf3124d0
Ubaid Khan <72931901+ubaidkhan321@users.noreply.github.com>
2025-02-26 09:59:12 +0000

    datatype add

--- 01_basic/index.js
+++ 01_basic/index.js
@@ -1,1 +1,22 @@
 console.log("Aneeque bhai");
+const accountemail = "account@gmail.com";
+let accountname = "Account";
+var accountpassword = 123456;
+accounttype = "Current";
+
+accountname = "Aneeque";
+accountpassword = "112233";
+accounttype = "Salary";
+  console.table([accountname,accountemail,accounttype,accountpassword]);
+console.log(typeof accounttype);
+
+//Data type on javascript
+//primitive data type
+//string
+//number
+//boolen
+//symbol
+//undefine
+//null
+//bigint
+
