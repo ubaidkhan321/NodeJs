f30fb3467bc06eebefb9064f1123de4a5d19b4ae
Ubaid Khan <72931901+ubaidkhan321@users.noreply.github.com>
2025-03-03 11:25:12 +0000

    function

---
+++ 01_basic/IIFE.js
@@ -0,0 +1,12 @@
+//Immediately Ivolk Fuctio Expression
+
+(function chai(){
+    console.log("DB CONNECTION")
+})();
+
+ ((num1,num2)=> 
+   console.log((num1 + num2)) )(2,2);
+
+((name)=>{
+    console.log(`my name is ${name}`)
+})("agha")
